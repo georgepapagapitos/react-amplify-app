19c42106fd58c6bdeea5033f051da6a2fd5ffe21
George Papagapitos <george.g.papagapitos@gmail.com>
2021-04-29 13:46:26 -0500

    removed typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Amplify!!!git
+          Hello from Amplify!!!
         </p>
         <a
           className="App-link"
